06332c9aeec8bdf7484256171e84186ce460e619
Michael Varvaruk <mvarvaruk@microsoft.com>
2024-05-06 12:25:34 +0300

    params.rs: handle param lifetimes, removed uint support

--- openssl/src/params.rs
+++ openssl/src/params.rs
@@ -1,7 +1,6 @@
 use std::ffi::CString;
 
 enum SupportedParamType {
-    Uint(c_unit),
     Utf8String(CString),
     Key(CString),
 }
@@ -22,31 +21,17 @@ impl OsslParams {
     }
 
     pub fn add_str(&mut self, key: &str, value: &str) -> &mut Self {
-        let key = SupportedParamType::Key(CString::new(key.as_bytes()).unwrap());
+        let key = CString::new(key.as_bytes()).unwrap();
 
         let value = CString::new(value.as_bytes()).unwrap();
         let raw_value = value.into_raw();
         let param = unsafe { ffi::OSSL_PARAM_construct_utf8_string(key.as_ptr(), raw_value, 0) };
 
-        self.lifetime_keep.push(key);
+        self.lifetime_keep.push(SupportedParamType::Key(key));
         unsafe {
             let kept_value = SupportedParamType::Utf8String(CString::from_raw(raw_value));
             self.lifetime_keep.push(kept_value);
         }
-
-        self.param_array.insert(0, param);
-
-        self
-    }
-
-    pub fn add_uint(&mut self, key: &str, value: u32) -> &mut Self {
-        let key = SupportedParamType::Key(CString::new(key.as_bytes()).unwrap());
-        let value = SupportedParamType::Uint(value);
-
-        let param = unsafe { ffi::OSSL_PARAM_construct_uint(key.as_ptr(), &mut value) };
-
-        self.lifetime_keep.push(key);
-        self.lifetime_keep.push(value);
 
         self.param_array.insert(0, param);
 
